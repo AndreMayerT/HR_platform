39becf8834a525774114e2e7185e4f305e633668
AndreMayerT <andremayerusa@gmail.com>
2023-09-25 15:39:31 -0300

    add create view for employee

--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import EmployeeList, EmployeeEdit, EmployeeDelete
+from .views import EmployeeList, EmployeeEdit, \
+ EmployeeDelete, EmployeeCreate
 
 urlpatterns = [
     path('', EmployeeList.as_view(), name='employee_list'),
+    path('create/', EmployeeCreate.as_view(), name='employee_create'),
     path('edit/<int:pk>', EmployeeEdit.as_view(), name='employee_update'),
     path('delete/<int:pk>', EmployeeDelete.as_view(), name='employee_delete')
 ]
--- apps/employees/views.py
+++ apps/employees/views.py
@@ -3,7 +3,8 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from .models import Employee
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -22,3 +23,17 @@ class EmployeeEdit(UpdateView):
 class EmployeeDelete(DeleteView):
     model = Employee
     success_url = reverse_lazy('employee_list')
+
+class EmployeeCreate(CreateView):
+    model = Employee
+    fields = ['name', 'departments']
+
+    def form_valid(self, form):
+        employee = form.save(commit=False)
+        employee.company = self.request.user.employee.company
+        employee.user = User.objects.create(
+            username=employee.name.split(' ')[0] + '.' +
+            employee.name.split(' ')[1])
+        employee.save()
+        return super(EmployeeCreate, self).form_valid(form)
+    
